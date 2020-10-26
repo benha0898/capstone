d661826765ac4e78a41c497511fe0be7b28e940c
estudillom <43586520+estudillom@users.noreply.github.com>
2020-10-26 11:24:18 -0600

    merge authentication and chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'authentication/signin.dart';
-import 'authentication/signup.dart';
-import 'customDatabase.dart';
-=======
+//import 'authentication/signin.dart';
+//import 'authentication/signup.dart';
+//import 'customDatabase.dart';
 
 import 'app.dart';
->>>>>>> 4affab761e12437b9dcf5622fc27e48a20c3cd38
 
 void main() => runApp(MyApp());
 
@@ -14,36 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.pink[400] ,
-            title: Text("Card Game App"),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.view_carousel)),
-                Tab(icon: Icon(Icons.message)),
-                Tab(icon: Icon(Icons.settings)),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              SignUp(),
-              CustomData(),
-              SignIn(),
-              Text("Settings Tab"),
-            ],
-          ),
-        ),
-=======
       title: "Conversation App",
       theme: ThemeData(
         primarySwatch: Colors.blue,
->>>>>>> 4affab761e12437b9dcf5622fc27e48a20c3cd38
       ),
       home: CustomNavigatorHomePage(),
     );
